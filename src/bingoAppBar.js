1c873d22560f2129bd4de5843d87a0f09c66e1bd
Alisa <alissa9090@mail.ru>
2020-09-25 00:55:37 +0200

    remove material ui from appBar

--- src/bingoAppBar.js
+++ src/bingoAppBar.js
@@ -1,20 +1,14 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable react/prop-types */
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 
 const ButtonAppBar = ({ startNewGame }) => (
   <div className="bingo-app-bar">
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" className="bingo-app-bar-title">
-          Funny Bingo
-        </Typography>
-        <Button className="new-game-button" onClick={startNewGame}>New game</Button>
-      </Toolbar>
-    </AppBar>
+    <h2 className="bingo-app-bar-title">
+      Funny Bingo
+    </h2>
+    <h3 className="new-game-button" onClick={startNewGame}>New game</h3>
   </div>
 );
 
