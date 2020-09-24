596d1c3c01436a496937c9300b08a6a7a72a7005
Alisa <alissa9090@mail.ru>
2020-09-25 00:20:34 +0200

    remove gap between cards

--- src/bingoCard.js
+++ src/bingoCard.js
@@ -1,12 +1,11 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
-import Paper from '@material-ui/core/Paper';
 
 // eslint-disable-next-line react/prop-types
 const BingoCard = ({ text, picked, onClick }) => (
   <div className="bingo-card-container responsive-font-size" onClick={onClick}>
-    <Paper className={`${picked ? 'crossed-out ' : ''}bingo-card-item`}>{text}</Paper>
+    <div className={`${picked ? 'crossed-out ' : ''}bingo-card-item`}>{text}</div>
   </div>
 );
 
