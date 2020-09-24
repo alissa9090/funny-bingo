258eeb1d5f5781c4fef420688d040b0d73bac88a
Alisa <alissa9090@mail.ru>
2020-09-25 00:02:04 +0200

    use own grid

--- src/bingoBoard.js
+++ src/bingoBoard.js
@@ -1,26 +1,20 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
 import BingoCard from './bingoCard';
 
 const BingoBoard = ({
   className, bingoCards, picked, pick
 }) => (
-  <Grid container spacing={1} justify="space-around" className={className}>
-    {[...Array(5).keys()].map(((row) => (
-
-      <Grid key={row} container item xs={12} spacing={1} justify="space-around">
-        {bingoCards.slice(row * 5, row * 5 + 5).map(({ id, text }) => (
-
-          <Grid key={id} item xs={2}>
-            <BingoCard text={text} picked={picked.includes(id)} onClick={pick(id)} />
-          </Grid>
-
-        ))}
-      </Grid>
-
-    )))}
-  </Grid>
+  <div className={`${className} bingo-board-container`}>
+    {bingoCards.map((bingoCard) => (
+      <BingoCard
+        key={bingoCard.id}
+        text={bingoCard.text}
+        picked={picked.includes(bingoCard.id)}
+        onClick={pick(bingoCard.id)}
+      />
+    ))}
+  </div>
 );
 
 export default BingoBoard;
